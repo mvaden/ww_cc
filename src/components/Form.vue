032984ef577c135144b1ada8fd44b2a214f073d9
Matthew Vaden <matthewvaden@gmail.com>
2018-05-09 17:31:34 -0400

    The form component.

---
+++ src/components/Form.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="form">
+    <h1>Hello World</h1>
+    <form id="form" @submit="checkForm" action="https://vuejs.org/" method="post">
+      <div>
+        <label for="name">Name:</label>
+        <input type="text" id="name" name="user_name" />
+      </div>
+      <div>
+        <label for="mail">E-mail:</label>
+        <input type="email" id="mail" name="user_mail" />
+      </div>
+      <div>
+        <label for="msg">Date of Birth:</label>
+        <input id="message" name="user_dob" />
+      </div>
+      <p>
+        <input type="submit" value="Submit" required/>  
+      </p>
+    </form> 
+  </div>
+</template>
+
+<script>
+export default {
+	name: 'Form'
+};
+</script>
